6472ab0623393898c0dd122120201b6fd311fc12
Ryan Marganti <rmarganti@gannett.com>
2021-09-14 08:47:37 -0400

    [nvim] Apply telescope.nvim layout_config properly.

--- editor/nvim.lua/lua/rmarganti/plugins/config/telescope.lua
+++ editor/nvim.lua/lua/rmarganti/plugins/config/telescope.lua
@@ -12,19 +12,21 @@ return function()
             }
         },
         dynamic_preview_title = true,
-        layout_config = {
-            center = {
-                preview_cutoff = 40
-            },
-            height = 0.9,
-            horizontal = {
-                preview_cutoff = 120,
-                prompt_position = "bottom"
-            },
-            vertical = {
-                preview_cutoff = 40
-            },
-            width = 0.95
+        defaults = {
+            layout_config = {
+                center = {
+                    preview_cutoff = 40
+                },
+                height = 0.9,
+                horizontal = {
+                    preview_cutoff = 120,
+                    prompt_position = "bottom"
+                },
+                vertical = {
+                    preview_cutoff = 40
+                },
+                width = 0.9
+            }
         }
     })
 
