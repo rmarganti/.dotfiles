91d8c4b07fe3d15613e62d2bb6b62b88af49c7a3
Ryan Marganti <rmarganti@gannett.com>
2021-09-17 08:41:34 -0400

    [nvim] Add default prettier config.

--- editor/nvim.lua/lua/rmarganti/plugins/config/efm/prettier.lua
+++ editor/nvim.lua/lua/rmarganti/plugins/config/efm/prettier.lua
@@ -1,4 +1,10 @@
 return {
     formatCommand = 'prettierd "${INPUT}"',
-    formatStdin = true
+    formatStdin = true,
+    env = {
+        string.format(
+            'PRETTIERD_DEFAULT_CONFIG=%s',
+            vim.fn.expand('~/.config/nvim/lua/rmarganti/plugins/config/efm/.prettierrc.json')
+        ),
+    },
 }
