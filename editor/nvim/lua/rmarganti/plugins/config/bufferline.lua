2145383b9b645bd3061411e40e6dd5cc93fb2546
Ryan Marganti <rmarganti@gannett.com>
2021-09-19 11:46:01 -0400

    [nvim] Customize nvim-bufferline.

--- editor/nvim/lua/rmarganti/plugins/config/bufferline.lua
+++ editor/nvim/lua/rmarganti/plugins/config/bufferline.lua
@@ -1,10 +1,17 @@
 return function()
 	require('bufferline').setup({
 		options = {
+            diagnostics = 'nvim_lsp',
+            diagnostics_indicator = function(_, level, _, _)
+                  local icon = level:match("error") and " " or " "
+                  return " " .. icon
+            end,
 			numbers = function(opts)
                 return string.format('%s.', opts.ordinal)
             end,
-			separator_style = 'slant'
+			separator_style = 'slant',
+            show_buffer_close_icons = false,
+            show_close_icon = false,
 		},
 	})
 end
