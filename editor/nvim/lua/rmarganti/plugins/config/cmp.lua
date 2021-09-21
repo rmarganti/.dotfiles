25f4498cad55ade9156d9cbf918ffb306d420b57
Ryan Marganti <rmarganti@gannett.com>
2021-09-21 15:23:20 -0400

    [nvim] Auto-complete with <CR>.

--- editor/nvim/lua/rmarganti/plugins/config/cmp.lua
+++ editor/nvim/lua/rmarganti/plugins/config/cmp.lua
@@ -3,6 +3,19 @@
 return function()
     local cmp = require('cmp')
 
+    local complete_or_fallback = cmp.mapping(
+        function(fallback)
+            if vim.fn.pumvisible() == 1 then
+                cmp.confirm({
+                    behavior = cmp.ConfirmBehavior.Insert,
+                    select = true,
+                })
+            else
+                fallback()
+            end
+        end
+    )
+
     cmp.setup({
         snippet = {
             expand = function(args)
@@ -13,18 +26,8 @@ return function()
             ['<C-d>'] = cmp.mapping.scroll_docs(-4),
             ['<C-f>'] = cmp.mapping.scroll_docs(4),
             ['<C-e>'] = cmp.mapping.close(),
-            ['<TAB>'] = cmp.mapping(
-                function(fallback)
-                    if vim.fn.pumvisible() == 1 then
-                        cmp.confirm({
-                            behavior = cmp.ConfirmBehavior.Insert,
-                            select = true,
-                        })
-                    else
-                        fallback()
-                    end
-                end
-            ),
+            ['<TAB>'] = complete_or_fallback,
+            ['<CR>'] = complete_or_fallback,
         },
         sources = {
             { name = 'nvim_lsp' },
