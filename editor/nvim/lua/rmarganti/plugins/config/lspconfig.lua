a048514f2bdd486632454263db1b4b3a1798aae2
Ryan Marganti <rmarganti@gannett.com>
2021-09-21 12:32:05 -0400

    [nvim] Simple dots for LSP error indicators.

--- editor/nvim/lua/rmarganti/plugins/config/lspconfig.lua
+++ editor/nvim/lua/rmarganti/plugins/config/lspconfig.lua
@@ -10,6 +10,25 @@ return function()
         }
     )
 
+    -- Set diagnostic icons.
+    local signs = {
+        Error = "●",
+        Warning = "●",
+        Hint = "●",
+        Information = "●"
+    }
+
+    for type, icon in pairs(signs) do
+        local hl = "LspDiagnosticsSign" .. type
+        vim.fn.sign_define(
+            hl, {
+            text = icon,
+            texthl = hl,
+            numhl = ""
+        })
+    end
+
+
     -- symbols for autocomplete
     vim.lsp.protocol.CompletionItemKind = {
         '   (Text) ',
