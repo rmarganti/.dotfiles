ece577c62ccad209d937e38612b3de346ce68cb0
Ryan Marganti <rmarganti@gannett.com>
2021-10-05 09:13:09 -0400

    [nvim] Rubber duck back, baby!

--- editor/nvim/lua/rmarganti/plugins/config/lualine.lua
+++ editor/nvim/lua/rmarganti/plugins/config/lualine.lua
@@ -1,7 +1,19 @@
 return function()
+    local function rubber_duck()
+        return ''
+    end
+
     require('lualine').setup({
         options = {
             theme = 'everforest',
-        }
+        },
+        sections = {
+            lualine_a = { rubber_duck, 'mode'},
+            lualine_b = {'branch', 'diff'},
+            lualine_c = {'filename'},
+            lualine_x = {'filetype'},
+            lualine_y = {'progress'},
+            lualine_z = {'location'},
+        },
     })
 end
