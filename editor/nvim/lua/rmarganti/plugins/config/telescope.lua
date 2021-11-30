121fdc61a8c16871ad20ec54c213df284f3a8e17
Ryan Marganti <rmarganti@gannett.com>
2021-11-30 10:36:40 -0500

    [nvim] Add multi-select and open to telescope.

--- editor/nvim/lua/rmarganti/plugins/config/telescope.lua
+++ editor/nvim/lua/rmarganti/plugins/config/telescope.lua
@@ -1,5 +1,49 @@
 local M = {}
 
+local actions = require('telescope.actions')
+local action_state = require('telescope.actions.state')
+local custom_actions = {}
+
+function custom_actions._multiopen(prompt_bufnr, open_cmd)
+    local picker = action_state.get_current_picker(prompt_bufnr)
+    local num_selections = #picker:get_multi_selection()
+
+    if num_selections > 1 then
+        vim.cmd("bw!") -- wipe the prompt buffer
+        for _, entry in ipairs(picker:get_multi_selection()) do
+            print(string.format("%s %s", open_cmd, entry.value))
+            vim.cmd(string.format("%s %s", open_cmd, entry.value))
+        end
+        vim.cmd('stopinsert')
+    else
+        if open_cmd == "vsplit" then
+            actions.file_vsplit(prompt_bufnr)
+        elseif open_cmd == "split" then
+            actions.file_split(prompt_bufnr)
+        elseif open_cmd == "tabe" then
+            actions.file_tab(prompt_bufnr)
+        else
+            actions.file_edit(prompt_bufnr)
+        end
+    end
+end
+
+function custom_actions.multi_selection_open_vsplit(prompt_bufnr)
+    custom_actions._multiopen(prompt_bufnr, "vsplit")
+end
+
+function custom_actions.multi_selection_open_split(prompt_bufnr)
+    custom_actions._multiopen(prompt_bufnr, "split")
+end
+
+function custom_actions.multi_selection_open_tab(prompt_bufnr)
+    custom_actions._multiopen(prompt_bufnr, "tabe")
+end
+
+function custom_actions.multi_selection_open(prompt_bufnr)
+    custom_actions._multiopen(prompt_bufnr, "edit")
+end
+
 -- https://github.com/nvim-telescope/telescope.nvim#telescope-defaults
 M.setup = function()
     local telescope = require('telescope')
@@ -15,6 +59,12 @@ M.setup = function()
         },
         dynamic_preview_title = true,
         defaults = {
+            dynamic_preview_title = true,
+            file_ignore_patterns = {
+                '^.git/',
+                '^node_modules/',
+                '^vendor/',
+            },
             layout_config = {
                 center = {
                     preview_cutoff = 40
@@ -29,12 +79,14 @@ M.setup = function()
                 },
                 width = 0.9
             },
-            file_ignore_patterns = {
-                '^.git/',
-                '^node_modules/',
-                '^vendor/',
-            },
-            dynamic_preview_title = true,
+            mappings = {
+                i = {
+                    ["<cr>"] = custom_actions.multi_selection_open,
+                },
+                n = {
+                    ["<cr>"] = custom_actions.multi_selection_open,
+                }
+            }
         },
     })
 end
