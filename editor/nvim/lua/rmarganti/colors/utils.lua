56ab514af419ae9e2240c46d04307804069b66da
Ryan Marganti <rmarganti@gannett.com>
2022-01-22 09:15:19 -0500

    --wip--

--- editor/nvim/lua/rmarganti/colors/utils.lua
+++ editor/nvim/lua/rmarganti/colors/utils.lua
@@ -29,10 +29,14 @@ M.highlight = function(group, color)
 	local style = color.style and "gui=" .. color.style or "gui=NONE"
 	local fg = color.fg and M.expand_color('fg', color.fg) or "guifg=NONE ctermfg=NONE"
 	local bg = color.bg and M.expand_color('bg', color.bg) or "guibg=NONE ctermbg=NONE"
-	local sp = color.sp and "guisp=" .. color.sp.gui or ""
-    local gui = color.gui and "gui=" .. color.gui or ""
-	local blend = color.blend and "blend=" .. color.blend or ""
-	local hl = "highlight " .. group .. " " .. style .. " " .. fg .. " " .. bg .. " " .. gui .. " " .. sp .. " " .. blend
+	local sp = color.sp and ("guisp=" .. color.sp.gui) or ""
+    local gui = color.gui and ("gui=" .. color.gui) or ""
+	local blend = color.blend and ("blend=" .. color.blend) or ""
+
+    local hl = table.concat(
+        { "highlight", group, style, fg, bg, gui, sp, blend },
+        " "
+    )
 
 	vim.cmd(hl)
 	if color.link then
