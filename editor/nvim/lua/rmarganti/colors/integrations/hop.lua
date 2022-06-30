f60bb34a51ecb229e75472f49c095ec360122a9e
Ryan Marganti <rmarganti@gannett.com>
2022-06-30 06:55:30 -0700

    [nvim] Tweak colorscheme for Hop.

--- editor/nvim/lua/rmarganti/colors/integrations/hop.lua
+++ editor/nvim/lua/rmarganti/colors/integrations/hop.lua
@@ -2,8 +2,8 @@ local a = require('rmarganti.colors.abstractions')
 local p = require('rmarganti.colors.palette')
 
 return {
-    HopNextKey = { bg = p.none, fg = a.plus2, style = "bold,underline" },
-    HopNextKey1 = { bg = p.none, fg = a.plus2, style = "bold" },
-    HopNextKey2 = { bg = p.none, fg = a.plus1, style = "bold,italic" },
-    HopUnmatched = { bg = p.none, fg = a.minus1 },
+    HopNextKey = { bg = p.none, fg = a.plus3, style = "bold" },
+    HopNextKey1 = { bg = p.none, fg = a.plus3, style="bold" },
+    HopNextKey2 = { bg = p.none, fg = a.base, style = "bold" },
+    HopUnmatched = { bg = p.none, fg = a.minus2 },
 }
