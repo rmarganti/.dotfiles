d997c9a2e953d32f493ce4d402a46215b2d94907
Ryan Marganti <rmarganti@gannett.com>
2022-07-26 13:21:32 -0400

    [nvim] Add Neovim API completions.

--- editor/nvim/lua/rmarganti/plugins/init.lua
+++ editor/nvim/lua/rmarganti/plugins/init.lua
@@ -250,6 +250,7 @@ packer.startup(function(use)
     use({ 'hrsh7th/cmp-nvim-lsp', after = 'lspkind-nvim' }) -- Loaded earlier for capabilities.
     use({ 'hrsh7th/cmp-buffer', after = 'nvim-cmp' })
     use({ 'hrsh7th/cmp-path', after = 'nvim-cmp' })
+    use({ 'hrsh7th/cmp-nvim-lua', after = 'nvim-cmp' })
     use({ 'tzachar/cmp-tabnine', run = './install.sh', after = 'nvim-cmp' })
     use({ 'saadparwaiz1/cmp_luasnip', after = 'nvim-cmp' })
     use({ 'andersevenrud/cmp-tmux', after = 'nvim-cmp' })
