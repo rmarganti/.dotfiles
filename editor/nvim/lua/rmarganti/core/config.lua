9aef9e05eaa439d020bb8a0db93be67b62c1aedb
Ryan Marganti <rmarganti@gannett.com>
2022-10-12 15:04:54 -0400

    [nvim] Disable mouse.

--- editor/nvim/lua/rmarganti/core/config.lua
+++ editor/nvim/lua/rmarganti/core/config.lua
@@ -4,7 +4,7 @@
 --
 ------------------------------------------------
 
-vim.opt.laststatus = 3 -- use global statusbar
+vim.opt.laststatus = 3 -- use global status bar
 
 if vim.fn.exists('+termguicolors') == 1 then
     vim.opt.termguicolors = true
@@ -15,6 +15,9 @@ end
 -- General Settings.
 --
 ------------------------------------------------
+
+-- Disable mouse.
+vim.opt.mouse = ''
 
 -- Show menus in UTF-8
 vim.opt.encoding = 'utf-8'
@@ -38,7 +41,7 @@ vim.opt.wildmenu = true
 -- Don't wrap lines.
 vim.opt.wrap = false
 
--- When wrap is enabled, only break at whitespace.
+-- When wrap is enabled, only break at white space.
 vim.opt.linebreak = true
 
 -- Disable backups, swaps, etc.
@@ -96,10 +99,11 @@ vim.opt.expandtab = true
 -- Be smart, obviously.
 vim.opt.smarttab = true
 
--- Auto indent, smart indent
+-- Hitting <CR> carries the current line's indentation to the next.
 vim.opt.autoindent = true
-vim.opt.smartindent = true
 
+-- When hitting <CR>, adjust indentation based on the current line's contents.
+vim.opt.smartindent = true
 
 ------------------------------------------------
 --
@@ -109,7 +113,6 @@ vim.opt.smartindent = true
 
 vim.opt.splitbelow = true
 vim.opt.splitright = true
-
 
 ------------------------------------------------
 --
