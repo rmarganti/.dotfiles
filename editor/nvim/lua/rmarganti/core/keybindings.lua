3e171e0202876445222c6263a59b06a3025f8176
Ryan Marganti <rmarganti@gannett.com>
2022-11-11 13:49:44 -0500

    [nvim] Git Checkout keybind reloads file.

--- editor/nvim/lua/rmarganti/core/keybindings.lua
+++ editor/nvim/lua/rmarganti/core/keybindings.lua
@@ -197,7 +197,7 @@ utils.map('n', '<Leader>ga', '<cmd>Git add % <bar> lua vim.notify("Git added cur
 utils.map(
     'n',
     '<Leader>go',
-    '<cmd>Git checkout % <bar> lua vim.notify("Git checked out current file")<CR>'
+    '<cmd>Git checkout % <bar> e! % <bar> lua vim.notify("Git checked out current file")<CR>'
 )
 
 -- Git Commits
