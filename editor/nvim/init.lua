8a3367d61ff8b80e82220dc01af88aae6ccc2d3f
Ryan Marganti <rmarganti@gannett.com>
2022-11-17 11:20:56 -0500

    [nvim] Don't skip netrw loading.

--- editor/nvim/init.lua
+++ editor/nvim/init.lua
@@ -17,7 +17,6 @@
 -- Disable some unused built-in Neovim plugins
 vim.g.loaded_man = false
 vim.g.loaded_gzip = false
-vim.g.loaded_netrwPlugin = false
 vim.g.loaded_tarPlugin = false
 vim.g.loaded_zipPlugin = false
 vim.g.loaded_2html_plugin = false
