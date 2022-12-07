b9da08b8b69f16b272fca1dc0c8db24674081680
Ryan Marganti <rmarganti@gannett.com>
2022-12-06 21:17:21 -0500

    [nvim] Update dressing config.

--- editor/nvim/lua/rmarganti/plugins/config/dressing.lua
+++ editor/nvim/lua/rmarganti/plugins/config/dressing.lua
@@ -4,15 +4,17 @@ M.config = function()
     require('dressing').setup({
         input = {
             insert_only = false,
-            winblend = 0,
             get_config = function(opts)
-                if (opts.kind == 'read_url') then
+                if opts.kind == 'read_url' then
                     return {
                         relative = 'win',
                         prefer_width = 60,
                     }
                 end
-            end
+            end,
+            win_options = {
+                winblend = 0,
+            },
         },
     })
 end
