4dfbaadd68dbb6b47d21813bf01d06a6ed4d4c9d
Ryan Marganti <rmarganti@gannett.com>
2023-01-19 23:52:17 -0500

    feat(nvim): Simplified Telescope highlights.

--- dots/.config/nvim/lua/rmarganti/colors/integrations/telescope.lua
+++ dots/.config/nvim/lua/rmarganti/colors/integrations/telescope.lua
@@ -2,13 +2,32 @@ local a = require('rmarganti.colors.abstractions')
 local p = require('rmarganti.colors.palette')
 
 return {
+    ------------------------------------------------
+    -- Defaults
+    ------------------------------------------------
+
     TelescopeNormal = { fg = p.fg, bg = a.float_bg },
     TelescopeBorder = { fg = a.border, bg = a.float_bg },
+
+    ------------------------------------------------
+    -- Preview
+    ------------------------------------------------
+
+    TelescopePreviewTitle = { fg = p.gray2, bg = p.bg_lighter },
+
+    ------------------------------------------------
+    -- Results
+    ------------------------------------------------
+
+    TelescopeResultsTitle = { fg = p.gray2, bg = p.bg_lighter },
     TelescopeSelectionCaret = { fg = a.select_bg },
     TelescopeSelection = { fg = a.select_fg, bg = a.select_bg },
     TelescopeMatching = { fg = p.cyan },
 
-    TelescopeResultsNormal = { bg = p.bg_light },
-    TelescopeResultsBorder = { fg = a.border, bg = p.bg_light },
-    TelescopeResultsTitle = { fg = a.border, bg = p.bg_light },
+    ------------------------------------------------
+    -- Prompt
+    ------------------------------------------------
+
+    TelescopePromptTitle = { fg = p.gray3, bg = p.bg_lighter },
+    TelescopePromptBorder = { fg = p.gray2, bg = a.float_bg, bold = true },
 }
