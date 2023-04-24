88854f922a7981cdef77ada0d8c561a9508fc82c
Ryan Marganti <rmarganti@gannett.com>
2023-04-24 09:33:09 -0400

    feat(nvim): Use dots for diagnostic indicators

--- dots/.config/nvim/lua/rmarganti/core/lsp.lua
+++ dots/.config/nvim/lua/rmarganti/core/lsp.lua
@@ -1,49 +1,39 @@
-vim.lsp.handlers['textDocument/publishDiagnostics'] = vim.lsp.with(
-    vim.lsp.diagnostic.on_publish_diagnostics,
-    {
+vim.lsp.handlers['textDocument/publishDiagnostics'] =
+    vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
         virtual_text = false,
         underline = true,
         signs = true,
-        update_in_insert = false
-    }
-)
+        update_in_insert = false,
+    })
 
-vim.lsp.handlers['textDocument/hover'] = vim.lsp.with(
-    vim.lsp.handlers.hover,
-    {
-        border = 'rounded'
-    }
-)
+vim.lsp.handlers['textDocument/hover'] = vim.lsp.with(vim.lsp.handlers.hover, {
+    border = 'rounded',
+})
 
-vim.lsp.handlers['textDocument/signatureHelp'] = vim.lsp.with(
-    vim.lsp.handlers.signature_help,
-    {
-        border = 'rounded'
-    }
-)
+vim.lsp.handlers['textDocument/signatureHelp'] = vim.lsp.with(vim.lsp.handlers.signature_help, {
+    border = 'rounded',
+})
 
 -- Set diagnostic icons.
 local signs = {
-    Error = '–',
-    Warn = '–',
-    Hint = '–',
-    Information = '–'
+    Error = '·',
+    Warn = '·',
+    Hint = '·',
+    Info = '·',
 }
 
 for type, icon in pairs(signs) do
     local hl = 'DiagnosticSign' .. type
-    vim.fn.sign_define(
-        hl, {
+    vim.fn.sign_define(hl, {
         text = icon,
         texthl = hl,
-        numhl = ''
+        numhl = '',
     })
 end
 
-
 vim.diagnostic.config({
     float = {
-        source = "always",  -- Or "if_many"
+        source = 'always', -- Or "if_many"
     },
     virtual_text = false,
 })
