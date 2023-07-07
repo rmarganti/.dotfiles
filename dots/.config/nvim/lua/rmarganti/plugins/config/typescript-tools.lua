9ea61c5fa00987329e4e3ea6897a1ba95055b0ef
Ryan Marganti <rmarganti@gannett.com>
2023-07-07 09:28:02 -0700

    feat(nvim): let typescript-tools find TS sever installation

--- dots/.config/nvim/lua/rmarganti/plugins/config/typescript-tools.lua
+++ dots/.config/nvim/lua/rmarganti/plugins/config/typescript-tools.lua
@@ -1,5 +1,4 @@
 local lsp_utils = require('rmarganti.plugins.config.lsp.lsp-utils')
-local util = require('lspconfig.util')
 
 local M = {
     'pmizio/typescript-tools.nvim',
@@ -9,10 +8,6 @@ local M = {
     },
     opts = {
         on_attach = lsp_utils.on_attach,
-        -- By default, typescript-tools will look for a `tsconfig.json`. In a
-        -- monorepo, this means it may set the root directory to that of the
-        -- package/app/library, but it will not find Typescript there.
-        root_dir = util.root_pattern('.git'),
     },
     event = 'VeryLazy',
 }
