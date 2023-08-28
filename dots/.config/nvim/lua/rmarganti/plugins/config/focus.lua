27dc79861463bf51d4b64308f411e201d3647eca
Ryan Marganti <rmarganti@gannett.com>
2023-08-28 17:02:54 -0400

    fix(nvim): use new focus.nvim config structure

--- dots/.config/nvim/lua/rmarganti/plugins/config/focus.lua
+++ dots/.config/nvim/lua/rmarganti/plugins/config/focus.lua
@@ -6,10 +6,23 @@ local M = {
 
 function M.config()
     require('focus').setup({
-        signcolumn = false,
-        excluded_filetypes = {
-            'fugitiveblame',
+        ui = {
+            signcolumn = false,
         },
+    })
+
+    local ignore_filetypes = { 'fugitiveblame' }
+
+    local augroup = vim.api.nvim_create_augroup('FocusDisable', { clear = true })
+
+    vim.api.nvim_create_autocmd('FileType', {
+        group = augroup,
+        callback = function(_)
+            if vim.tbl_contains(ignore_filetypes, vim.bo.filetype) then
+                vim.b.focus_disable = true
+            end
+        end,
+        desc = 'Disable focus autoresize for FileType',
     })
 end
 
