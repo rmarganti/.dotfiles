5280b09f56ac09e8de5028cabc8ecdd122683892
Ryan Marganti <rmarganti@gannett.com>
2023-08-31 11:26:05 -0400

    feat(nvim): change typescript comment snippets

--- dots/.config/nvim/lua/rmarganti/core/statuscolumn.lua
+++ dots/.config/nvim/lua/rmarganti/core/statuscolumn.lua
@@ -1,4 +1,4 @@
 vim.opt.statuscolumn = [[%{(foldlevel(v:lnum) && foldlevel(v:lnum) > foldlevel(v:lnum - 1)) ? (foldclosed(v:lnum) == -1 ? '⌄' : '›') : ' '}]] -- Folds
     .. ' ' -- Spacer
     .. '%=%{v:virtnum < 1 ? (v:relnum ? v:relnum : v:lnum) : " "}' -- Line number
-    .. '%s' -- Status
+    .. '%s' -- Sign
