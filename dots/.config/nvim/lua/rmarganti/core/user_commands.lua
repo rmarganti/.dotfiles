6d1e4bbd4893acc7c40c02ffa933bde5bb405cc7
Ryan Marganti <rmarganti@gannett.com>
2023-12-05 14:04:14 -0500

    feat(nvim): FormatJSON user command

--- dots/.config/nvim/lua/rmarganti/core/user_commands.lua
+++ dots/.config/nvim/lua/rmarganti/core/user_commands.lua
@@ -1,15 +1,11 @@
 local core_fns = require('rmarganti.core.functions')
 
-vim.api.nvim_create_user_command(
-    'EditNearest',
-    function(opts)
-        core_fns.edit_nearest(opts.args)
-    end,
-    { nargs = 1, force = true }
-)
+vim.api.nvim_create_user_command('EditNearest', function(opts)
+    core_fns.edit_nearest(opts.args)
+end, { nargs = 1, force = true })
 
-vim.api.nvim_create_user_command(
-    'ReadURL',
-    core_fns.read_url,
-    { nargs = 0, force = true }
-)
+vim.api.nvim_create_user_command('ReadURL', core_fns.read_url, { nargs = 0, force = true })
+
+vim.api.nvim_create_user_command('FormatJSON', function()
+    vim.cmd('%!jq "."')
+end, { nargs = 0, force = true })
