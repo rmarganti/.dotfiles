af2af9d284b3bf3625602a03e66d96392a0eb549
Ryan Marganti <rmarganti@gannett.com>
2023-12-28 10:04:22 -0500

    feat(nvim): neverforest support for hydra.nvim

---
+++ dots/.config/nvim/lua/rmarganti/colors/integrations/hydra.lua
@@ -0,0 +1,9 @@
+local p = require('rmarganti.colors.palette')
+
+return {
+    HydraRed = { fg = p.red },
+    HydraBlue = { fg = p.blue },
+    HydraAmaranth = { fg = p.red_light },
+    HydraTeal = { fg = p.cyan },
+    HydraPink = { fg = p.magenta },
+}
