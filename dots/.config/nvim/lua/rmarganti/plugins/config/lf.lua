655fc2f61c46351d3205d70d6a9c365c1c766ada
Ryan Marganti <rmarganti@gannett.com>
2024-03-27 14:04:18 -0400

    feat(nvim): remove custom keybinds from lf

--- dots/.config/nvim/lua/rmarganti/plugins/config/lf.lua
+++ dots/.config/nvim/lua/rmarganti/plugins/config/lf.lua
@@ -15,15 +15,6 @@ function M.config()
         border = 'rounded',
         winblend = 0,
     })
-
-    vim.api.nvim_create_autocmd('User', {
-        pattern = 'LfTermEnter',
-        callback = function(a)
-            -- `q` and `esc` exit the terminal
-            vim.api.nvim_buf_set_keymap(a.buf, 't', 'q', 'q', { nowait = true })
-            vim.api.nvim_buf_set_keymap(a.buf, 't', '<esc>', 'q', { nowait = true })
-        end,
-    })
 end
 
 return M
