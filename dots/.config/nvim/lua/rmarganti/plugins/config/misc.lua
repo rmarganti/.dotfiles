94f9a8e7d7b1d404260ccbc8a6900c6deeb4619c
Ryan Marganti <rmarganti@gannett.com>
2024-06-14 09:42:17 -0400

    navigator

---
+++ dots/.config/nvim/lua/rmarganti/plugins/config/misc.lua
@@ -0,0 +1,11 @@
+return {
+    --------------------------------
+    -- Misc
+    --------------------------------
+
+    -- Navigate between Neovim and Wezterm panes
+    {
+        'numToStr/Navigator.nvim',
+        opts = {},
+    },
+}
