363df0c7d0c555600bf7f6e1193a58020f910f5b
Ryan Marganti <rmarganti@gannett.com>
2024-06-17 10:04:14 -0400

    obsidian.nvim

--- dots/.config/nvim/lua/rmarganti/plugins/config/telescope.lua
+++ dots/.config/nvim/lua/rmarganti/plugins/config/telescope.lua
@@ -6,8 +6,7 @@ local M = {
         { 'nvim-lua/plenary.nvim' },
         { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
         -- { 'nvim-telescope/telescope-github.nvim' },
-        -- { 'nvim-telescope/telescope-symbols.nvim' },
-        -- { 'princejoogie/dir-telescope.nvim' },
+        { 'princejoogie/dir-telescope.nvim' },
         -- { 'rcarriga/nvim-notify' },
         -- { 'rlch/github-notifications.nvim' },
         { 'smartpde/telescope-recent-files' },
@@ -20,75 +19,57 @@ function M.config()
     telescope.setup({
         extensions = {
             fzf = {
-                fuzzy = true,                   -- false will only do exact matching
+                fuzzy = true, -- false will only do exact matching
                 override_generic_sorter = true, -- override the generic sorter
-                override_file_sorter = true,    -- override the file sorter
-                case_mode = 'smart_case',       -- or "ignore_case" or "respect_case"
+                override_file_sorter = true, -- override the file sorter
+                case_mode = 'smart_case', -- or "ignore_case" or "respect_case"
             },
         },
 
-        -- defaults = {
-        --     layout_strategy = 'vertical',
-        --     dynamic_preview_title = true,
-        --     file_ignore_patterns = {
-        --         '^.git/',
-        --         '^node_modules/',
-        --         '^vendor/',
-        --         'yarn.lock',
-        --     },
-        --     layout_config = {
-        --         center = {
-        --             preview_cutoff = 40,
-        --         },
-        --         height = 0.9,
-        --         horizontal = {
-        --             preview_cutoff = 120,
-        --             prompt_position = 'bottom',
-        --         },
-        --         vertical = {
-        --             preview_cutoff = 40,
-        --             preview_height = 25,
-        --         },
-        --         width = 0.9,
-        --     },
-        --     prompt_prefix = '   ',
-        --     vimgrep_arguments = {
-        --         'rg',
-        --         '--color=never',
-        --         '--no-heading',
-        --         '--with-filename',
-        --         '--line-number',
-        --         '--column',
-        --         '--smart-case',
-        --         '--hidden',
-        --     },
-        -- },
+        defaults = {
+            layout_strategy = 'vertical',
+            dynamic_preview_title = true,
+            file_ignore_patterns = {
+                '^.git/',
+                '^node_modules/',
+                '^vendor/',
+                'yarn.lock',
+            },
+            layout_config = {
+                center = {
+                    preview_cutoff = 40,
+                },
+                height = 0.9,
+                horizontal = {
+                    preview_cutoff = 120,
+                    prompt_position = 'bottom',
+                },
+                vertical = {
+                    preview_cutoff = 40,
+                    preview_height = 25,
+                },
+                width = 0.9,
+            },
+            prompt_prefix = '   ',
+            vimgrep_arguments = {
+                'rg',
+                '--color=never',
+                '--no-heading',
+                '--with-filename',
+                '--line-number',
+                '--column',
+                '--smart-case',
+                '--hidden',
+            },
+        },
     })
 
-    -- require('telescope').load_extension('dir')
+    require('telescope').load_extension('dir')
     require('telescope').load_extension('fzf')
     -- require('telescope').load_extension('gh')
     -- require('telescope').load_extension('ghn')
     -- require('telescope').load_extension('notify')
-    -- require('telescope').load_extension('recent_files')
-
-    -- local augroup = vim.api.nvim_create_augroup('TelescopeCustom', { clear = true })
-    --
-    -- vim.api.nvim_create_autocmd({ 'VimEnter', 'WinEnter', 'BufWinEnter', 'BufEnter' }, {
-    --     group = augroup,
-    --     pattern = '*',
-    --     callback = function(ev)
-    --         -- Telescope sets the file type after the fact,
-    --         -- so we use `vim.schedule` to give it time to do so.
-    --         vim.schedule(function()
-    --             local filetype = vim.api.nvim_buf_get_option(ev.buf, 'filetype')
-    --             if filetype == 'TelescopePrompt' then
-    --                 vim.opt_local.cursorline = false
-    --             end
-    --         end)
-    --     end,
-    --     desc = 'Disables cursorline for Telescope',
-    -- })
+    require('telescope').load_extension('recent_files')
 end
 
 return M
