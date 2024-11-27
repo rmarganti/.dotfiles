f1f6eeefd15cc584a49f54a6c08cdb9e1b29d323
Ryan Marganti <rmarganti@gannett.com>
2024-11-27 11:35:15 -0500

    feat(nvim): terraform colorscheme additions

--- dots/.config/nvim/lua/rmarganti/colors/languages/init.lua
+++ dots/.config/nvim/lua/rmarganti/colors/languages/init.lua
@@ -1,4 +1,5 @@
 return {
     markdown = require('rmarganti.colors.languages.markdown'),
+    terraform = require('rmarganti.colors.languages.terraform'),
     vimwiki = require('rmarganti.colors.languages.vimwiki'),
 }
---
+++ dots/.config/nvim/lua/rmarganti/colors/languages/terraform.lua
@@ -0,0 +1,5 @@
+local a = require('rmarganti.colors.abstractions')
+
+return {
+    ['@lsp.type.type.terraform'] = { fg = a.base },
+}
