60257d487fe7a9a42dcb4688e649ba3c20d7aeec
Ryan Marganti <rmarganti@gannett.com>
2024-11-29 15:39:55 -0500

    feat(nvim): render color after text

--- dots/.config/nvim/lua/rmarganti/plugins/config/nvim-highlight-colors.lua
+++ dots/.config/nvim/lua/rmarganti/plugins/config/nvim-highlight-colors.lua
@@ -3,6 +3,8 @@ return {
     event = 'VeryLazy',
     opts = {
         virtual_symbol = '●',
+        virtual_symbol_suffix = '',
+        virtual_symbol_position = 'eow',
         render = 'virtual',
     },
 }
