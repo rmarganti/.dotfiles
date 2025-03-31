6f9532219263cbf313c8dbb10b9b55449d6600f9
Ryan Marganti <ryan.marganti@gmail.com>
2025-03-31 00:45:53 -0400

    feat(nvim): mode-consistent lualine highlights

--- dots/.config/nvim/lua/lualine/themes/neverforest.lua
+++ dots/.config/nvim/lua/lualine/themes/neverforest.lua
@@ -9,31 +9,31 @@ return {
 
     insert = {
         a = { bg = p.bg_lightest.gui, fg = p.green.gui, gui = 'bold' },
-        b = { bg = p.bg_lighter.gui, fg = p.gray_dark.gui },
+        b = { bg = p.bg.gui, fg = p.gray_dark.gui },
         c = { bg = p.bg_light.gui, fg = p.gray_dark.gui },
     },
 
     visual = {
         a = { bg = p.bg_lightest.gui, fg = p.gray.gui, gui = 'bold' },
-        b = { bg = p.bg_lighter.gui, fg = p.gray_dark.gui },
+        b = { bg = p.bg.gui, fg = p.gray_dark.gui },
         c = { bg = p.bg_light.gui, fg = p.gray_dark.gui },
     },
 
     replace = {
         a = { bg = p.bg_lightest.gui, fg = p.green.gui, gui = 'bold' },
-        b = { bg = p.bg_lighter.gui, fg = p.gray_dark.gui },
+        b = { bg = p.bg.gui, fg = p.gray_dark.gui },
         c = { bg = p.bg_light.gui, fg = p.gray_dark.gui },
     },
 
     command = {
         a = { bg = p.bg_lightest.gui, fg = p.yellow.gui, gui = 'bold' },
-        b = { bg = p.bg_lighter.gui, fg = p.gray_dark.gui },
+        b = { bg = p.bg.gui, fg = p.gray_dark.gui },
         c = { bg = p.bg_light.gui, fg = p.gray_dark.gui },
     },
 
     inactive = {
         a = { bg = p.bg_lightest.gui, fg = p.magenta.gui, gui = 'bold' },
-        b = { bg = p.bg_lighter.gui, fg = p.gray_dark.gui },
+        b = { bg = p.bg.gui, fg = p.gray_dark.gui },
         c = { bg = p.bg_light.gui, fg = p.gray_dark.gui },
     },
 }
