267061d1729b38fd59a63bdb46f591d1ec43783d
Ryan Marganti <ryan.marganti@gmail.com>
2025-03-31 00:38:56 -0400

    feat(nvim): subtler lualine styles

--- dots/.config/nvim/lua/lualine/themes/neverforest.lua
+++ dots/.config/nvim/lua/lualine/themes/neverforest.lua
@@ -3,37 +3,37 @@ local p = require('rmarganti.colors.palette')
 return {
     normal = {
         a = { bg = p.bg_lightest.gui, fg = p.gray.gui, gui = 'bold' },
-        b = { bg = p.bg_lighter.gui, fg = p.gray.gui },
-        c = { bg = p.bg_light.gui, fg = p.gray.gui }
+        b = { bg = p.bg.gui, fg = p.gray_dark.gui },
+        c = { bg = p.bg_light.gui, fg = p.gray_dark.gui },
     },
 
     insert = {
-        a = { bg = p.green.gui, fg = p.black.gui, gui = 'bold' },
-        b = { bg = p.bg_lighter.gui, fg = p.gray.gui },
-        c = { bg = p.bg_light.gui, fg = p.gray.gui }
+        a = { bg = p.bg_lightest.gui, fg = p.green.gui, gui = 'bold' },
+        b = { bg = p.bg_lighter.gui, fg = p.gray_dark.gui },
+        c = { bg = p.bg_light.gui, fg = p.gray_dark.gui },
     },
 
     visual = {
-        a = { bg = p.gray_light.gui, fg = p.black.gui, gui = 'bold' },
-        b = { bg = p.bg_lighter.gui, fg = p.gray.gui },
-        c = { bg = p.bg_light.gui, fg = p.gray.gui }
+        a = { bg = p.bg_lightest.gui, fg = p.gray.gui, gui = 'bold' },
+        b = { bg = p.bg_lighter.gui, fg = p.gray_dark.gui },
+        c = { bg = p.bg_light.gui, fg = p.gray_dark.gui },
     },
 
     replace = {
-        a = { bg = p.green.gui, fg = p.black.gui, gui = 'bold' },
-        b = { bg = p.bg_lighter.gui, fg = p.gray.gui },
-        c = { bg = p.bg_light.gui, fg = p.gray.gui }
+        a = { bg = p.bg_lightest.gui, fg = p.green.gui, gui = 'bold' },
+        b = { bg = p.bg_lighter.gui, fg = p.gray_dark.gui },
+        c = { bg = p.bg_light.gui, fg = p.gray_dark.gui },
     },
 
     command = {
-        a = { bg = p.yellow.gui, fg = p.black.gui, gui = 'bold' },
-        b = { bg = p.bg_lighter.gui, fg = p.gray.gui },
-        c = { bg = p.bg_light.gui, fg = p.gray.gui }
+        a = { bg = p.bg_lightest.gui, fg = p.yellow.gui, gui = 'bold' },
+        b = { bg = p.bg_lighter.gui, fg = p.gray_dark.gui },
+        c = { bg = p.bg_light.gui, fg = p.gray_dark.gui },
     },
 
     inactive = {
-        a = { bg = p.gray.magenta, fg = p.black.gui, gui = 'bold' },
-        b = { bg = p.bg_lighter.gui, fg = p.gray.gui },
-        c = { bg = p.bg_light.gui, fg = p.gray.gui }
-    }
- }
+        a = { bg = p.bg_lightest.gui, fg = p.magenta.gui, gui = 'bold' },
+        b = { bg = p.bg_lighter.gui, fg = p.gray_dark.gui },
+        c = { bg = p.bg_light.gui, fg = p.gray_dark.gui },
+    },
+}
--- dots/.config/nvim/lua/rmarganti/plugins/config/lualine.lua
+++ dots/.config/nvim/lua/rmarganti/plugins/config/lualine.lua
@@ -9,14 +9,19 @@ local M = {
     config = function()
         require('lualine').setup({
             options = {
-                component_separators = { left = '', right = '' },
+                component_separators = { left = '', right = '' },
                 section_separators = { left = '', right = '' },
             },
 
             sections = {
                 lualine_a = { 'mode' },
-                lualine_b = { 'branch', 'diff', 'diagnostics' },
-                lualine_c = { 'filename' },
+                lualine_b = {
+                    {
+                        'branch',
+                        separator = { right = '' },
+                    },
+                },
+                lualine_c = { 'diff', 'diagnostics', 'filename' },
                 lualine_x = { 'encoding', 'fileformat', 'filetype' },
                 lualine_y = { 'progress' },
                 lualine_z = { 'location' },
