5d3cc05f76b84c35b2cd273a33c2b56b54f7f381
Ryan Marganti <rmarganti@gannett.com>
2025-04-21 15:25:38 -0400

    feat(nvim): truncate mode in statusbar

--- dots/.config/nvim/lua/rmarganti/plugins/config/lualine.lua
+++ dots/.config/nvim/lua/rmarganti/plugins/config/lualine.lua
@@ -14,7 +14,14 @@ local M = {
             },
 
             sections = {
-                lualine_a = { 'mode' },
+                lualine_a = {
+                    {
+                        'mode',
+                        fmt = function(str)
+                            return str:sub(1, 3)
+                        end,
+                    },
+                },
                 lualine_b = {
                     {
                         'branch',
