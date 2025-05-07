64c81e894b2bf19131da323e647f0bbd0e2b8f93
Ryan Marganti <rmarganti@gannett.com>
2025-05-07 15:09:10 -0400

    feat(nvim): use vim.diagnostic.config for setting diagnostic sign text

--- dots/.config/nvim/lua/rmarganti/core/lsp.lua
+++ dots/.config/nvim/lua/rmarganti/core/lsp.lua
@@ -34,6 +34,13 @@ end
 vim.diagnostic.config({
     float = {
         source = 'always', -- Or "if_many"
+        signs = {
+            text = {
+                [vim.diagnostic.severity.ERROR] = '·',
+                [vim.diagnostic.severity.WARN] = '·',
+                [vim.diagnostic.severity.HINT] = '·',
+                [vim.diagnostic.severity.INFO] = '·',
+            },
+        },
     },
-    virtual_text = false,
 })
