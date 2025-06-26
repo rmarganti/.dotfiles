d6c216f1699ec9613d0801667aa6407750616023
Ryan Marganti <rmarganti@gannett.com>
2025-06-26 09:38:41 -0400

    feat(nvim): 0.11 lsp configs

--- dots/.config/nvim/lua/rmarganti/core/lsp.lua
+++ dots/.config/nvim/lua/rmarganti/core/lsp.lua
@@ -6,41 +6,16 @@ vim.lsp.handlers['textDocument/publishDiagnostics'] =
         update_in_insert = false,
     })
 
-vim.lsp.handlers['textDocument/hover'] = vim.lsp.with(vim.lsp.handlers.hover, {
-    border = 'rounded',
-})
-
-vim.lsp.handlers['textDocument/signatureHelp'] = vim.lsp.with(vim.lsp.handlers.signature_help, {
-    border = 'rounded',
-})
-
--- Set diagnostic icons.
-local signs = {
-    Error = '·',
-    Warn = '·',
-    Hint = '·',
-    Info = '·',
-}
-
-for type, icon in pairs(signs) do
-    local hl = 'DiagnosticSign' .. type
-    vim.fn.sign_define(hl, {
-        text = icon,
-        texthl = hl,
-        numhl = '',
-    })
-end
-
 vim.diagnostic.config({
     float = {
-        source = 'always', -- Or "if_many"
-        signs = {
-            text = {
-                [vim.diagnostic.severity.ERROR] = '·',
-                [vim.diagnostic.severity.WARN] = '·',
-                [vim.diagnostic.severity.HINT] = '·',
-                [vim.diagnostic.severity.INFO] = '·',
-            },
+        source = true, -- Or "if_many"
+    },
+    signs = {
+        text = {
+            [vim.diagnostic.severity.ERROR] = '·',
+            [vim.diagnostic.severity.WARN] = '·',
+            [vim.diagnostic.severity.HINT] = '·',
+            [vim.diagnostic.severity.INFO] = '·',
         },
     },
 })
