84b6d0814f54d458fac92d6bda982ff6eab58e66
Ryan Marganti <rmarganti@gannett.com>
2025-12-05 12:46:02 -0500

    feat(zed): lf task highlights current file

--- dots/.config/zed/scripts/zed_lf.sh
+++ dots/.config/zed/scripts/zed_lf.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
-start_dir="${ZED_FILE%/*}"
-start_dir="${start_dir:-${ZED_WORKTREE_ROOT:-.}}"
+start_path="${ZED_FILE:-${ZED_WORKTREE_ROOT:-.}}"
 selection_file=$(mktemp)
 
-lf -selection-path="$selection_file" "$start_dir"
+lf -selection-path="$selection_file" "$start_path"
 [ -s "$selection_file" ] && zed "$(cat "$selection_file")"
 
 rm "$selection_file"
