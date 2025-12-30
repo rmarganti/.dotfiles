82b64ac7b9ce2d951b195484c72c7de1409c5c7c
Ryan Marganti <rmarganti@gannett.com>
2025-12-30 09:43:51 -0500

    feat(zed): lf script allows multiple file selection

--- dots/.config/zed/scripts/zed_lf.sh
+++ dots/.config/zed/scripts/zed_lf.sh
@@ -3,6 +3,6 @@ start_path="${ZED_FILE:-${ZED_WORKTREE_ROOT:-.}}"
 selection_file=$(mktemp)
 
 lf -selection-path="$selection_file" "$start_path"
-[ -s "$selection_file" ] && zed "$(cat "$selection_file")"
+[ -s "$selection_file" ] && cat "$selection_file" | tr '\n' '\0' | xargs -0 zed
 
 rm "$selection_file"
