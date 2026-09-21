cc0908b6ae279d97b8b3b633fd185809d0c5401c
Ryan Marganti <rmarganti@gannett.com>
2026-09-21 12:23:39 -0400

    feat(nvim): replace hydra win mini.clue

--- dots/.config/nvim/lua/rmarganti/colors/integrations/hydra.lua
+++
@@ -1,9 +0,0 @@
-local p = require('rmarganti.colors.palette')
-
-return {
-    HydraRed = { fg = p.red },
-    HydraBlue = { fg = p.blue },
-    HydraAmaranth = { fg = p.red_light },
-    HydraTeal = { fg = p.cyan },
-    HydraPink = { fg = p.magenta },
-}
